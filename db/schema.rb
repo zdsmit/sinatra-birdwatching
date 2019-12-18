ca513d4f6bbe831e89ff8b112ba7b50dc955b6f6
Zach  Smith <zdsmith001@gmail.com>
2019-12-18 17:27:05 +0000

    added datetime and location to sightings table, added password to users table, created sightings index view

--- db/schema.rb
+++ db/schema.rb
@@ -19,10 +19,13 @@ ActiveRecord::Schema.define(version: 2019_12_17_190935) do
   create_table "sightings", force: :cascade do |t|
     t.integer "user_id"
     t.integer "bird_id"
+    t.datetime "time"
+    t.string "place"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
   end
 
 end
