7f1b93ce697fc64b83921dd0b2ca18869d9ca6cc
Zach  Smith <zdsmith001@gmail.com>
2019-12-30 18:02:42 +0000

    started to create update function in sightings controller, created edit page

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -11,7 +11,12 @@ class SightingsController < ApplicationController
 
   get "/sightings/:id" do
     @sighting = Sighting.find_by_id(params[:id])
-    erb "/sightings/show_sighting"
+    erb :'/sightings/show_sighting'
+  end
+
+  get "/sightings/:id/edit" do
+    @sighting = Sighting.find_by_id(params[:id])
+    erb :'/sightings/update_sighting'
   end
 
   post '/new' do
@@ -21,6 +26,19 @@ class SightingsController < ApplicationController
     @sighting.bird = @bird
     @sighting.save
     redirect to "/sightings"
+  end
+
+  patch "/sightings/:id" do
+    if params[:bird] == "" || params[:place] == "" || params[:time] == ""
+        redirect to "/sightings/#{params[:id]}/edit"
+    else
+      @sighting = Sighting.find_by_id(params[:id])
+      if @sighting.update(bird: params[:bird], place: params[:place], time: params[:time])
+        redirect to "/sightings/#{@sighting.id}"
+      else
+        redirect to "/sightings/#{params[:id]}/edit"
+      end
+    end
   end
 
 end
